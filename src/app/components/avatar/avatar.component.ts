6833690e82817c1165c1d8dd3753e7c2460af669
Gabriel Sena <git@gabrielsena.dev>
2024-07-13 20:08:21 -0300

    feat: update dashboard layout

--- src/app/components/avatar/avatar.component.ts
+++ src/app/components/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, computed } from '@angular/core';
 import { FirstLetterPipe } from './first-letter.pipe';
 import { MatCardAvatar, MatCardModule } from '@angular/material/card';
 
@@ -12,8 +12,13 @@ import { MatCardAvatar, MatCardModule } from '@angular/material/card';
 export class AvatarComponent {
   @Input({ required: false }) imageUrl: string | null = null;
   @Input({ required: true }) name: string = '';
+  @Input() width: string = '24px';
+  @Input() height: string = '24px';
+
+  avatarVariableStyles = computed(() => `--avatar-width: ${this.width}; --avatar-height: ${this.height}`);
 
   hasImageUrl() {
     return !!this.imageUrl;
   }
+
 }
