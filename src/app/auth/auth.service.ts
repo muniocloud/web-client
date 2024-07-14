8619da3681b65eda1103d371839d2c244f4cb0b2
Gabriel Sena <git@gabrielsena.dev>
2024-07-14 16:30:07 -0300

    feat: create session creator

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,17 +7,21 @@ import { Router } from '@angular/router';
 type User = {
   name: string;
   avatarUrl: string | null;
-}
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   private currentUserSubject: BehaviorSubject<User | null>;
   private currentUser: Observable<User | null>;
   isLoading = signal(false);
 
-  constructor(private http: HttpClient, @Inject(BASE_URL) private baseUrl: string, private router: Router) {
+  constructor(
+    private http: HttpClient,
+    @Inject(BASE_URL) private baseUrl: string,
+    private router: Router
+  ) {
     this.isLoading.set(true);
     this.currentUserSubject = new BehaviorSubject<User | null>(null);
     this.currentUser = this.currentUserSubject.asObservable();
@@ -42,41 +46,50 @@ export class AuthService {
       return;
     }
 
-    (await this.getUser()).pipe(finalize(() => {
-      this.isLoading.set(false);
-    }))
-    .subscribe(value => {
-      this.currentUserSubject.next(value);
-      if (value) {
-        return;
-      }
-    });
+    const userObserver = await this.getUser();
+
+    userObserver
+      .pipe(
+        finalize(() => {
+          this.isLoading.set(false);
+        })
+      )
+      .subscribe((value) => {
+        this.currentUserSubject.next(value);
+        if (value) {
+          return;
+        }
+      });
   }
 
-  getUser() {
+  private getUser() {
     return this.http.get<User>(`${this.baseUrl}/user`);
   }
 
   login(email: string, password: string) {
-    return this.http.post<any>(`${this.baseUrl}/auth/login`, { email, password }).pipe(
-      map(async (data) => {
-        localStorage.setItem('user-token', data.accessToken);
-        this.loadUser();
-        this.router.navigate(['/']);
-        return true;
-      }),
-    );
+    return this.http
+      .post<any>(`${this.baseUrl}/auth/login`, { email, password })
+      .pipe(
+        map(async (data) => {
+          localStorage.setItem('user-token', data.accessToken);
+          this.loadUser();
+          this.router.navigate(['/']);
+          return true;
+        })
+      );
   }
 
   signup(email: string, password: string, name: string) {
-    return this.http.post<any>(`${this.baseUrl}/auth/signup`, { name, email, password }).pipe(
-      map(async (data) => {
-        localStorage.setItem('user-token', data.accessToken);
-        this.loadUser();
-        this.router.navigate(['/']);
-        return true;
-      }),
-    );
+    return this.http
+      .post<any>(`${this.baseUrl}/auth/signup`, { name, email, password })
+      .pipe(
+        map(async (data) => {
+          localStorage.setItem('user-token', data.accessToken);
+          this.loadUser();
+          this.router.navigate(['/']);
+          return true;
+        })
+      );
   }
 
   logout() {
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, inject } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, Inject, ViewChild, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -12,7 +12,13 @@ import { MatDivider } from '@angular/material/divider';
 import { AvatarComponent } from "../../components/avatar/avatar.component";
 import { AuthService } from '../../auth/auth.service';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlider, MatSliderModule } from '@angular/material/slider';
+import { HttpClient } from '@angular/common/http';
+import { BASE_URL } from '../../shared/api/base-url.provider';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -32,6 +38,9 @@ import { RouterOutlet } from '@angular/router';
     AvatarComponent,
     CommonModule,
     RouterOutlet,
+    MatSelectModule,
+    MatSliderModule,
+    MatProgressBar,
 ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
@@ -39,7 +48,43 @@ import { RouterOutlet } from '@angular/router';
 export class DashboardComponent {
   authService = inject(AuthService);
 
+  isLoading = signal(false);
+
+  sessionCreatorForm = new FormGroup({
+    difficult: new FormControl('1', [
+      Validators.required,
+    ]),
+    lessons: new FormControl('3', [
+      Validators.required,
+    ]),
+    context: new FormControl('routine', [
+      Validators.required,
+    ]),
+  });
+
   @ViewChild('drawer') drawer!: MatDrawer;
+  
+  constructor(
+    private http: HttpClient,
+    @Inject(BASE_URL) private baseUrl: string,
+    private router: Router,
+  ) {}
+
+  createSession() {
+    this.isLoading.set(true);
+    this.http.post<{
+      sessionId: number;
+      lessonsIds: number[];
+    }>(`${this.baseUrl}/sessions`, {
+      level: +(this.sessionCreatorForm.value.difficult ?? 1),
+      lessons: +(this.sessionCreatorForm.value.lessons ?? 2),
+      context: this.sessionCreatorForm.value.context,
+    })
+    .pipe(finalize(() => this.isLoading.set(false)))
+    .subscribe(({ sessionId, lessonsIds } ) => {
+      this.router.navigate([`sessions/${sessionId}/lessons/${lessonsIds[0]}`]);
+    });
+  }
 
   public toggleDrawer() {
     this.drawer.toggle();
