b7e0643f0e3956afbc9ad4f4306fc121e6b33a80
Gabriel Sena <git@gabrielsena.dev>
2024-07-17 14:21:51 -0300

    feat: update environment configurations

--- src/app/shared/providers/base-url.provider.ts
+++ src/app/shared/providers/base-url.provider.ts
@@ -1,8 +1,7 @@
 import { InjectionToken } from "@angular/core";
-import { environment } from "../../../environments/environment.development";
+import { environment } from "../../../environments/environment";
 
 export const BASE_URL: InjectionToken<string> = new InjectionToken<string>('baseURL');
 
 const apiUrl = environment.apiUrl;
-
 export const baseUrlProvider = { provide: BASE_URL, useValue: apiUrl };
